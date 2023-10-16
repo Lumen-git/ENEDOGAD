0f302ea654a02f318b9b3f61908adce6dc3f0e88
lumen-git <aimoran@protonmail.com>
2023-10-15 21:19:31 -0500

    Created Power Up template

---
+++ EuclidsDungeon/Assets/PowerUps/PowerUpTemplate.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpTemplate : MonoBehaviour
+{
+    //TEMPALTE VARS
+    public float bobbingSpeed = 2.0f;  // Speed of bobbing motion.
+    public float bobbingAmount = 0.2f; // Amount of bobbing motion.
+    public float rotationSpeed = 30.0f; // Speed of rotation.
+    private Vector3 initialPosition;
+    private float time = 0f;
+    //TEMPLATE VARS
+
+    void Start()
+    {
+        //TEMPLATE START
+        initialPosition = transform.position;
+        //TEMPLATE START
+    }
+
+
+    void Update()
+    {
+        //TEMPLATE UPDATE
+        time += Time.deltaTime;
+        Vector3 newPosition = initialPosition;
+        newPosition.y += Mathf.Sin(time * bobbingSpeed) * bobbingAmount;
+        transform.position = newPosition;
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        //TEMPLATE UPDATE
+    }
+
+    private void OnTriggerEnter(Collider other){
+            if (other.gameObject.tag == "Player"){
+                //DO SOMETHING
+                Destroy(gameObject);
+            }
+        }
+
+}
