1bcb38bdfcc83b0eeeca49f4fa406304e5cbb95f
lumen-git <aimoran@protonmail.com>
2023-10-26 17:51:30 -0400

    its like minecraft but from hell :D

--- Invader/Assets/ModularRooms/ProcGen/ProcGen.cs
+++ Invader/Assets/ModularRooms/ProcGen/ProcGen.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; //IDK what this is but the internet says it works and i don't dare touch it
 
 public class ProcGen : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Transform[] children;
+
+    private float cooldownTime = 1f; // Cooldown time in seconds
+    private bool isOnCooldown = false;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
+
+    private void OnTriggerEnter(Collider other){
+        if (!isOnCooldown){ //The player has 2 hitboxes that handles different things. This makes sure one one triggers this method
+            isOnCooldown = true;
+            StartCoroutine(ResetCooldown());
+
+        
+        if (other.gameObject.tag == "Player"){
+            children = GetComponentsInChildren<Transform>();
+
+            if (transform != null && children.Length > 0){  //exclude parent itself
+                children = children.Where(child => child != transform).ToArray();
+            }
+
+            foreach (Transform child in children){  //For each child, which are the spawn nodes
+                Debug.Log("Child name: " + child.name);
+            }
+
+        }}
+
+    }
+    
+
+    private IEnumerator ResetCooldown(){
+        yield return new WaitForSeconds(cooldownTime);
+        isOnCooldown = false;
+    }
+
 }
